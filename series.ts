c45be70a0365780a4447975e04889dcb485f3aa4
bsalcedosuarez <123487830+bsalcedosuarez@users.noreply.github.com>
2024-11-27 09:16:14 -0500

    Create series.ts

---
+++ series.ts
@@ -0,0 +1,56 @@
+[
+    {
+        "id": 1,
+        "name": "Breaking Bad",
+        "platform": "AMC",
+        "seasons": 5,
+        "description": "Set and filmed in Albuquerque, New Mexico, the series tells the story of Walter White, a struggling and depressed high school chemistry teacher who is diagnosed with lung cancer",
+        "official_link": "https://www.amc.com/shows/breaking-bad",
+        "image": "https://i.imgur.com/GGje0vc.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Orange Is the New Black",
+        "platform": "Netflix",
+        "seasons": 6,
+        "description": "The series begins revolving around Piper Chapman, a woman in her thirties living in New York City who is sentenced to 15 months in Litchfield Penitentiary",
+        "official_link": "https://www.netflix.com/co/title/70242311",
+        "image": "https://i.imgur.com/EvKe48G.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Game of Thrones",
+        "platform": "HBO",
+        "seasons": 7,
+        "description": "American fantasy drama",
+        "official_link": "https://www.hbo.com/game-of-thrones",
+        "image": "https://i.imgur.com/TDCEV1S.jpg"
+    },
+    {
+        "id": 4,
+        "name": "The Big Bang Theory",
+        "platform": "CBS",
+        "seasons": 12,
+        "description": "Leonard and Sheldon are brilliant physicists but socially challenged. They interact with Penny and friends, while juggling careers, relationships, and family life.",
+        "official_link": "https://www.cbs.com/shows/big_bang_theory/about/",
+        "image": "https://i.imgur.com/uAEpVWk.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Sherlock",
+        "platform": "BBC",
+        "seasons": 4,
+        "description": "Sherlock depicts consulting detective Sherlock Holmes solving various mysteries in modern-day London. Assisted by his flatmate Dr. John Watson.",
+        "official_link": "https://www.bbc.co.uk/programmes/b018ttws",
+        "image": "https://i.imgur.com/02B7qhj.jpg"
+    },
+    {
+        "id": 6,
+        "name": "A Very English Scandal",
+        "platform": "BBC",
+        "seasons": 2,
+        "description": "A fact-based British television comedy-drama miniseries based on John Preston's book of the same name.",
+        "official_link": "https://www.bbc.co.uk/programmes/p065smy4",
+        "image": "https://i.imgur.com/D4y3DrQ.jpg"
+    }
+]
